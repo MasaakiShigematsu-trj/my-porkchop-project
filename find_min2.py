4553911c311e4f9832e61427a67e46d49b3b9b8c
MasaakiShigematsu-trj <shigematsu.trajectory@gmail.com>
2025-04-29 21:07:34 +0900

    04292107

--- find_min2.py
+++ find_min2.py
@@ -4,24 +4,24 @@ from astropy.time import Time
 # データを読み込む（nanを正しく読み取る）
 data = np.loadtxt('result_f2.txt')
 
-# 7列目（インデックス6）のデータを取り出す
+# 3列目（インデックス2）のデータと7列目（インデックス6）のデータを取り出す
+col3 = data[:, 2]
 col7 = data[:, 6]
 
-# NaNを除外して最小値を探す
-valid_indices = ~np.isnan(col7)  # NaNでない場所だけTrue
-col7_valid = col7[valid_indices]
+# 3列目の値が250以下かつ、7列目の値が最小となる行を探す
+valid_rows = (col3 <= 250) & (~np.isnan(col7))  # 3列目が250以下かつ7列目がNaNでない行
+
+# 条件を満たす行から7列目の最小値を探す
+valid_data = data[valid_rows]  # 条件を満たすデータを抽出
+col7_valid = valid_data[:, 6]  # 7列目のデータ
 
 # 最小値のインデックスを探す
 min_index_valid = np.argmin(col7_valid)
 
-# 元のdataに対応するインデックスに直す
-valid_rows = np.where(valid_indices)[0]
-min_row_index = valid_rows[min_index_valid]
-
 # 最小値を持つ行を取得
-min_row = data[min_row_index]
+min_row = valid_data[min_index_valid]
 
-print("7列目が最小の行：")
+print("3列目が250以下で7列目が最小の行：")
 print(min_row)
 
 # ユリウス日を西暦に変換
